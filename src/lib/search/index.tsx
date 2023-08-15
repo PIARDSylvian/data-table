410bba32f3fc2205fa1b2be31085f281a8a8abb3
PIARDSylvian <piard.sylvian@gmail.com>
2023-08-15 16:04:00 +0200

    add prettierrc & format files

--- src/lib/search/index.tsx
+++ src/lib/search/index.tsx
@@ -1,24 +1,29 @@
 import style from './style.module.scss'
 
 interface Props {
-  search: string
-  changeSearch: (data: string)=>void
+    search: string
+    changeSearch: (data: string) => void
 }
 
 /**
  * Render Search
- * 
+ *
  * @param {Props} props
  * @param {string} props.search
  * @param {Function} props.changeSearch
- * 
+ *
  * @returns {JSX.Element}
  */
-export default function Search({search, changeSearch}: Props): JSX.Element {
-  return (
-    <div className={style['table-search']}>
-      <label htmlFor="table-search">Search:</label>
-      <input type="search" id="table-search" value={search} onChange={(e) => changeSearch(e.target.value)} />
-    </div>
-  )
+export default function Search({ search, changeSearch }: Props): JSX.Element {
+    return (
+        <div className={style['table-search']}>
+            <label htmlFor="table-search">Search:</label>
+            <input
+                type="search"
+                id="table-search"
+                value={search}
+                onChange={(e) => changeSearch(e.target.value)}
+            />
+        </div>
+    )
 }
--- src/lib/search/search.test.tsx
+++ src/lib/search/search.test.tsx
@@ -1,16 +1,18 @@
-import React from 'react';
-import { fireEvent, render, screen } from '@testing-library/react';
-import Search from './index';
+import React from 'react'
+import { fireEvent, render, screen } from '@testing-library/react'
+import Search from './index'
 import '@testing-library/jest-dom'
 
-let search: string | null = null;
-const changeSearch = (newSearch: string) => {search = newSearch}
+let search: string | null = null
+const changeSearch = (newSearch: string) => {
+    search = newSearch
+}
 
 describe('Search integration test', () => {
-  it('should by render', () => {
-    render(<Search search={''} changeSearch={changeSearch} />)
-    const input = screen.getByLabelText('Search:')
-    fireEvent.change(input, {target: {value: 'valueToSearch'}})
-    expect(search).toEqual('valueToSearch')
-  });
+    it('should by render', () => {
+        render(<Search search={''} changeSearch={changeSearch} />)
+        const input = screen.getByLabelText('Search:')
+        fireEvent.change(input, { target: { value: 'valueToSearch' } })
+        expect(search).toEqual('valueToSearch')
+    })
 })
